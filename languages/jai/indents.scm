ccc3f2af43e3370df0ac553f4b756a9ab1c11dde
Chip Collier <chip@slowgames.eu>
2025-08-28 08:06:42 +0200

    Add basic auto-indentation queries

---
+++ languages/jai/indents.scm
@@ -0,0 +1,6 @@
+(block "{" @start "}" @end) @indent
+(struct_or_union_block "{" @start "}" @end) @indent
+(enum_declaration "{" @start "}" @end) @indent
+
+(_ "(" @start ")" @end) @indent
+(_ "[" @start "]" @end) @indent
